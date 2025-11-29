b275c0895ac3295799f7402cb8f919dd598daa5d
AnSuHan <glorygem195@gmail.com>
2025-11-29 13:26:49 +0900

    Spring 프로젝트 생성
    jdk21 & IntelliJ IDEA Community Edition 2025.2.5

---
+++ demo/src/test/java/com/spring/y20251129/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.y20251129.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
